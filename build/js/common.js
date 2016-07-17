6d4ab9e32ccd25add41e7e9a10bbfb16ec6df00a
Ex-Zy <serega1905@ukr.net>
2016-07-17 21:24:42 +0300

    upd

--- build/js/common.js
+++ build/js/common.js
@@ -1,3 +1,33 @@
 $(document).ready(function() {
 
+	function dropDown () {
+		$('.js-dropdown').click(function() {
+			var that     = $(this),
+				item     = that.parents('.list'),
+				allItems = $('.list'),
+				open     = item.hasClass('is-open'),
+				time     = 300;
+
+			if(!open) {
+				allItems.removeClass('is-open').find('.list__content').stop(true).slideUp(time);
+				item.addClass('is-open').find('.list__content').stop(true).slideDown(time);
+			} else {
+				item.removeClass('is-open').find('.list__content').stop(true).slideUp(time);
+			}
+
+		});
+	}
+
+	function winSize () {
+		var win = $(window).width() <= 767;
+
+		if(win) {
+			dropDown();
+		} else {
+			$('.list').removeClass('is-open');
+			$('.js-dropdown').unbind();
+		}
+	}
+
+	$(window).on('load resize', winSize);
 });
